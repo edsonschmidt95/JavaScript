c42f659792198876eaf8bb4879107dc3cbbfb3b6
Edson Augusto Schmidt <dinho.a.schmidt@hotmail.com>
2023-08-29 17:15:48 -0300

    ex010

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var v=60
+console.log (`a veloidade do seu carro é de ${v}km/h`)
+if (v>60){
+    console.log (`limite da via é de 60km/h, você passou a ${v}km/h. MULTADO`)
+}
+console.log(`Dirija sempre usando o conto de segunrança!`)
