1e7d0f37fc2c99ac5e5535348aba78627ef4b6b2
Edson Augusto Schmidt <dinho.a.schmidt@hotmail.com>
2023-08-31 17:38:26 -0300

    testes

--- aula12/exercicios/ex015/script.js
+++ aula12/exercicios/ex015/script.js
@@ -3,9 +3,5 @@ function verificar(){
     var ano = data.getFullYear()
     var fano=document.getElementById('txtano')
     var res= document.getElementById('res')
-    if (fano.Value.length == 0 || fano.Value >ano){
-        window.alert('[ERRO] verefique data')
-    }else{
-        window.alert('tudo ok')
-    }
+    if (fano.ariaValueMax)
 }
